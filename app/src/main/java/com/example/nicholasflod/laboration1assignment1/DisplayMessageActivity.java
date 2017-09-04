0b4a27c1e0a6560e3b3ae44cacd1e1ae1f8a3d0c
flodare <Nicholas.flod@gmail.com>
2017-09-04 19:43:52 +0200

    Kan nu fylla i ett textfält och skicka det till en ny aktivitet. Har även ändrat i manifestet för min "DisplayMessageActivity" så att en bakåt-knapp finns i menyfältet

---
+++ app/src/main/java/com/example/nicholasflod/laboration1assignment1/DisplayMessageActivity.java
@@ -0,0 +1,19 @@
+package com.example.nicholasflod.laboration1assignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String messageFromMain = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(messageFromMain);
+    }
+}
--- app/src/main/java/com/example/nicholasflod/laboration1assignment1/MainActivity.java
+++ app/src/main/java/com/example/nicholasflod/laboration1assignment1/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.nicholasflod.laboration1assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
-    private int hej = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
