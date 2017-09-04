670c53d8df4b3802708f9c957252e389ceba6033
flodare <Nicholas.flod@gmail.com>
2017-09-04 19:13:55 +0200

    Olika layouts för landscape och portrait mode

---
+++ app/src/main/java/com/example/nicholasflod/laboration1assignment1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nicholasflod.laboration1assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int hej = 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
